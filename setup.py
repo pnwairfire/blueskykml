48bc888103050b1419e489095569fd9507170a62
Joel Dubowy <jdubowy@gmail.com>
2015-07-21 16:33:53 -0700

    Use numpy 1.8.0 to be consistent with vacuum

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     url='https://github.com/pnwairfire/blueskykml/',
     description='Package for creating kmls from BlueSky smoke dispersion output.',
     install_requires=[
-        "numpy==1.9.2",
+        "numpy==1.8.0",
         "GDAL==1.11.2",
         "Pillow==2.8.1",
         "matplotlib==1.4.3"
