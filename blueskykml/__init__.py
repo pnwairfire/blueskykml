7a6c2f415531f1cb24c017c17a3808d7546c2bf1
Joel Dubowy <jdubowy@gmail.com>
2016-02-03 13:14:15 -0800

    Bump version to v0.2.8

--- blueskykml/__init__.py
+++ blueskykml/__init__.py
@@ -1,5 +1,5 @@
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
 
-__version_info__ = (0,2,7)
+__version_info__ = (0,2,8)
 __version__ = '.'.join([str(n) for n in __version_info__])
